d262b3e0fa7ed6c034ca6d5371e47d089cbf11e5
Răzvan Pavel <razvan.e.pavel@gmail.com>
2021-11-12 15:29:46 +0200

    Add support for Google Analytics
    Co-authored-by: Ciprian Platica <platica.ciprian@gmail.com>

--- src/main.js
+++ src/main.js
@@ -1,12 +1,27 @@
-import { createApp } from 'vue';
-import VueVirtualScroller from 'vue-virtual-scroller';
+import { createApp } from "vue";
 
-import 'tailwindcss/tailwind.css';
+import VueVirtualScroller from "vue-virtual-scroller"
+import VueGtag from "vue-gtag";
 
-import App from './App.vue';
-import router from './router';
+import "tailwindcss/tailwind.css";
+
+import App from "./App.vue"
+import router from "./router"
 
 createApp(App)
   .use(router)
   .use(VueVirtualScroller)
-  .mount('#app');
+  .use(VueGtag, {
+    appName: "vieti-pierdute",
+    pageTrackerScreenviewEnabled: true,
+    pageTrackerTemplate(to) {
+      return {
+        page_title: to.name + ' ' + to.path,
+        page_path: to.path
+      }
+    },
+    config: {
+      id: process.env.VUE_APP_ANALYTICS_MEASUREMENT_ID
+    }
+  }, router)
+  .mount("#app")
